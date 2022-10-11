af869d603e823161e71dde81a9e02fb9a0801849
miksskim55 <miksskim55@gmail.com>
2022-10-11 12:34:01 +0300

    Решение первой задачи, с исправлением.

--- src/lessons/iterables/task_1.py
+++ src/lessons/iterables/task_1.py
@@ -2,14 +2,15 @@ s = str(input())
 s_new = [str(i) for i in s]
 s_p = []
 for j in range(len(s_new)):
-    if s_new[j] == '(' or s_new[j] == '[' or s_new[j] == '{':
+    if s_new[j] in {'(', '{', '['}:
         s_p.append(s_new[j])
         continue
-    if s_new[j] == ')' or s_new[j] == ']' or s_new[j] == '}':
-        if s_p[-1] + s_new[j] == '()' or s_p[-1] + s_new[j] == '[]' or s_p[-1] + s_new[j] == '{}':
+    if s_new[j] in {')', '}', ']'}:
+        if len(s_p) >= 1 and s_p[-1] + s_new[j] in {'()', '{}', '[]'}:
             s_p.pop()
         else:
+            s_p.append(s_new[j])
             print('no')
+            break
 if s_p == []:
     print('yes')
-
