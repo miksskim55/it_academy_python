cbdb1555bb419be7ae317e8937cbb153d302e5cb
miksskim55 <miksskim55@gmail.com>
2022-10-18 22:54:22 +0300

    Новое решение второй задачи

--- src/lessons/iterables/task_2.py
+++ src/lessons/iterables/task_2.py
@@ -1,27 +1,38 @@
-name, add_param = input('Введите автора и дополнительный параметр: ').split(' ', 1)
-min_or_max = {}
-names_for_not = []
-novels_for_not = []
-years_for_not = []
-ss = []
 import csv
+def read_input():
+    input_string = input('Введите автора и дополнительный параметр: ')
+    return input_string.split(' ', 1)
 
-with open('library.csv') as f:
-    csvfile = csv.DictReader(f, delimiter=' ', quotechar='|', fieldnames=['names', 'novels', 'years', 'topics'])
-    for row in csvfile:
-        names = row.get('names').split(',')
-        names_for_not.append(names[0])
-        novels_for_not.append(row.get('novels'))
-        years_for_not.append(row.get('years'))
-        if name in names[0] and add_param in row.get('years'):
-            print(row.get('names'), row.setdefault('novels'), row.get('years'))
-        if name in names[0] and add_param in row.get('novels'):
-            print(row.get('names'), row.get('novels'), row.get('years'))
-        if name in names[0]:
-            min_or_max[row.get('years')] = row.get('novels')
-            if add_param == 'first':
-                print(row.get('names'), row.get('novels'), min(min_or_max.keys()))
-            elif add_param == 'last':
-                print(row.get('names'), row.get('novels'), max(min_or_max.keys()))
-if name not in names_for_not or add_param not in novels_for_not and add_param not in years_for_not and add_param not in {'first', 'last'}:
-    print('Такого писателя нет в библиотеке, или проверьте корректность введенных данных.')
+def read_file(library, novel_author):
+    library = []
+    with open('library.csv') as f:
+        csvfile = csv.DictReader(f, delimiter=' ', quotechar='|', fieldnames=['name', 'novel', 'year', 'topic'])
+        for row in csvfile:
+            names = row.get('name').split(',')
+            if row['name'].split(',')[0] == novel_author:
+                library.append(row)
+    return library
+
+def main():
+    novel_author, add_param = read_input()
+    library = read_file('library.csv', novel_author)
+
+    if add_param == 'first':
+        first = min(library, key=lambda row: row['year'])
+        print(', '.join( i for i in first.values()))
+    elif add_param == 'last':
+        last = max(library, key=lambda row: row['year'])
+        print(', '.join( i for i in last.values()))
+    elif next((x for x in library if x['year'] in add_param), None):
+        year = next((x for x in library if x['year'] in add_param), None)
+        print(', '.join(i for i in year.values()))
+    elif next((x for x in library if x['novel'] in add_param), None):
+        dict_library = {}
+        for i in library:
+            dict_library.update(i)
+            if add_param in dict_library.get('novel'):
+                print(', '.join(i for i in dict_library.values()))
+    else:
+        print('Такого писателя нет в библиотеке, или проверьте корректность введенных данных.')
+if __name__ == '__main__':
+    main()
